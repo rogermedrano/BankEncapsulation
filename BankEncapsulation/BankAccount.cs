b7ef54da52589c6dcf1be143af63fd33d1384db3
Roger Medrano <email@example.com>
2021-11-10 10:14:02 -0700

    1

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+
+        public void Deposit(double amountToDeposit)
+        {
+            //Define a method named Deposit that will accept a double and store that value in the balance field
+            
+            Console.Write("Enter the amount you wish to deposit:  $");
+            double currentDeposit = Convert.ToInt32(Console.ReadLine());
+            
+            balance = balance += currentDeposit;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+        //public void Withdrawal(double amountOfWithdrawal)
+        //{
+        //    //Define a method named Deposit that will accept a double and store that value in the balance field
+        //    balance -= amountOfWithdrawal;
+        //}
+
+        //public double GetBalance1()
+        //{
+        //    return balance;
+        //}
+
+
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myAccount = new BankAccount();
+
+            //Deposit();
+
+            myAccount.Deposit(0);
+            var balance = myAccount.GetBalance();
+
+            Console.WriteLine($"Your new balance is:  ${balance}");
+            Console.WriteLine();
+            Console.WriteLine();
+            {
+                Console.WriteLine("Would you like to make another deposit?");
+                string answer = Console.ReadLine();
+                if (answer == "yes")
+                {
+                    myAccount.Deposit(0);
+                }
+                if (answer == "no")
+                {
+                    Console.WriteLine("Have a good day.");
+                }
+                else
+                {
+                    Console.WriteLine("It's a YES or NO question... Log back in and try again.");
+                }
+            }
+        }
+
+        
+
+    }
+}
